36a3d2ce7fa272440252d1f25c8ea93caee9b7e8
DEV <dev@info.local>
2020-11-13 16:32:29 -0300

    exercicio 2 finalizado

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   Console.WriteLine("============================");
+            Console.WriteLine("Digite seu ano de nascimento");
+            Console.WriteLine("============================");
+
+
+            int ano = int.Parse(Console.ReadLine());
+
+            int idade = 2020-ano;
+            double semanas = idade*52.178;
+
+            Console.Write("Você viveu aproximadamente "+idade+" anos E "+semanas+" semanas");
+        }
+    }
+}
